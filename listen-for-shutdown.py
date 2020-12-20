2f940f9d54d6a8f9507a85e1f1479b945675b413
Andres Dans <andresdans@gmail.com>
2020-12-20 20:51:51 +0100

    Update listen-for-shutdown.py
    Added the turn off the led just before shutting down

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
-
+#
+#Button on GPIO 24
+#LED on GPIO 27
 
 import RPi.GPIO as GPIO
 import subprocess
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.wait_for_edge(24, GPIO.FALLING)
+
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
+GPIO.output(27, GPIO.LOW) # Turn off the led
+
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
